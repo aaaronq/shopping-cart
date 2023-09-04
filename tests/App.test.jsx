b6bd41e58193b2ec58478a9cbe4d089c843e40bc
aaaronq <aaronquigley@mail.com>
2023-09-04 22:40:52 +0100

    Add redirect file for netlify

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "../src/components/App";
 
 describe("App", () => {
 	it("renders header", () => {
-		render(<App />);
+		render(<MemoryRouter> <App /> </MemoryRouter>);
 		expect(screen.getByRole("heading").textContent).toMatch(
-			/Vite \+ React/i
+			/Mall Grab/i
 		);
 	});
 });
